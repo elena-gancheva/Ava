59313439aef2deffc88d4841d316349d4cc2dd13
Antony Dikov <antony.dikov@live.com>
2017-02-16 21:17:55 +0200

    [Fix] Small fix.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-const hbs = require('hbs');
 
 const passport = require('passport');
 const User = require('./models/user-model');
